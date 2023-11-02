2b15cc266c96b700354f37e8b7c590d4a1ff23eb
Sang Hoon Kim <KSHoon522@gamil.com>
2023-11-02 17:36:05 +0900

    Mutation Resolvers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { ApolloServer, gql } from "apollo-server";
 
-const tweets = [
+let tweets = [
   {
     id: "1",
     text: "first",
@@ -36,8 +36,29 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
+    allTweets() {
+      return tweets;
+    },
     tweet(root, { id }) {
       return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) {
+        return false;
+      }
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
     },
   },
 };
