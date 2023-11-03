2ac9f08f9a395e41a016660a847c48d9368b5559
Sang Hoon Kim <KSHoon522@gamil.com>
2023-11-03 11:15:07 +0900

    Type resolvers / Relationships

--- server.js
+++ server.js
@@ -4,38 +4,67 @@ let tweets = [
   {
     id: "1",
     text: "first",
+    userId: "2",
   },
   {
     id: "2",
     text: "second",
+    userId: "1",
+  },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "nico",
+    lastName: "las",
+  },
+  {
+    id: "2",
+    firstName: "Sang",
+    lastName: "hoon",
   },
 ];
 
 const typeDefs = gql`
+  """
+  Tweet object represents a resource for a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
-    auther: User
+    author: User
   }
   type User {
     id: ID!
-    username: String!
     firstName: String!
-    lastName: String
+    lastName: String!
+    """
+    fullName is the sum of firstName and lastName
+    """
+    fullName: String!
   }
   type Query {
+    allUsers: [User!]!
     allTweets: [Tweet!]!
     tweet(id: ID!): Tweet
     ping: String!
   }
   type Mutation {
     postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if i found, else return false
+    """
     deleteTweet(id: ID!): Boolean!
   }
 `;
 
 const resolvers = {
   Query: {
+    allUsers() {
+      console.log("allUsers");
+      return users;
+    },
     allTweets() {
       return tweets;
     },
@@ -48,10 +77,17 @@ const resolvers = {
       const newTweet = {
         id: tweets.length + 1,
         text,
+        userId,
       };
-      tweets.push(newTweet);
-      return newTweet;
+      const idCheck = users.find((user) => user.id === userId);
+      if (idCheck) {
+        tweets.push(newTweet);
+        return newTweet;
+      } else {
+        console.log("no user");
+      }
     },
+
     deleteTweet(_, { id }) {
       const tweet = tweets.find((tweet) => tweet.id === id);
       if (!tweet) {
@@ -59,6 +95,18 @@ const resolvers = {
       }
       tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
+    },
+  },
+  User: {
+    fullName(root) {
+      console.log("fullName");
+      console.log(root);
+      return root.firstName + root.lastName;
+    },
+  },
+  Tweet: {
+    author(root) {
+      return users.find((user) => user.id === root.userId);
     },
   },
 };
